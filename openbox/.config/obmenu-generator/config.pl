d923349a26688bddd2e3484b5194e474f462e11f
Filip Hesslund <filip@hesslund.org>
2016-03-23 18:15:53 +0100

    added openbox config

---
+++ openbox/.config/obmenu-generator/config.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+our $CONFIG = {
+  "editor"              => "gedit",
+  "Linux::DesktopFiles" => {
+                             desktop_files_paths     => ["/usr/share/applications"],
+                             gtk_rc_filename         => "/home/filip/.gtkrc-2.0",
+                             icon_dirs_first         => undef,
+                             icon_dirs_last          => undef,
+                             icon_dirs_second        => undef,
+                             keep_unknown_categories => 1,
+                             skip_entry              => undef,
+                             skip_filename_re        => undef,
+                             skip_svg_icons          => 0,
+                             strict_icon_dirs        => undef,
+                             substitutions           => undef,
+                             terminalization_format  => "%s -e '%s'",
+                             terminalize             => 1,
+                             unknown_category_key    => "other",
+                           },
+  "missing_icon"        => "gtk-missing-image",
+  "name_keys"           => ["Name"],
+  "terminal"            => "st",
+  "VERSION"             => 0.66,
+}
