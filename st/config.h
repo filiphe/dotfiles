428b8fafd19c865b2e4501f673630eafc839779b
Filip Hesslund <filip@hesslund.org>
2016-05-23 17:37:49 +0200

    changed st font

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Mononoki:pixelsize=14:antialias=true:autohint=false";
+static char font[] = "Fantasque Sans Mono:pixelsize=16:antialias=true:autohint=false";
 static int borderpx = 0;
 #define histsize 2000
 
