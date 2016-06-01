25167df265819a47657bb333911254e5d1f5a7c7
Filip Hesslund <filip@hesslund.org>
2016-06-01 09:28:12 +0200

    rename variable

--- scripts/.bin/create-ten-latest-ocremix-playlist.sh
+++ scripts/.bin/create-ten-latest-ocremix-playlist.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-SOURCE_PLAYLIST_FILE=$1
+TARGET_PLAYLIST_FILE=$1
 TMP_PLAYLIST_FILE="/tmp/playlist"
 OCREMIX_SINGLES_DIR="$HOME/media/music/OCREMIX/singles"
 
 ls -lArt "$OCREMIX_SINGLES_DIR" | tail -n 10 | rev | cut -d ' ' -f 1 | rev | cut -d '*' -f 1 | tac > "$TMP_PLAYLIST_FILE"
-sed -e 's#^#OCREMIX/singles/#' "$TMP_PLAYLIST_FILE" > "$SOURCE_PLAYLIST_FILE"
+sed -e 's#^#OCREMIX/singles/#' "$TMP_PLAYLIST_FILE" > "$TARGET_PLAYLIST_FILE"
 rm "$TMP_PLAYLIST_FILE"
