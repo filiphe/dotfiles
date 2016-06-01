5c3a80a53ba1e22be5092fca60e4fac5d9c089cd
Filip Hesslund <filip@hesslund.org>
2016-06-01 10:18:49 +0200

    update script to use `find` instead of `ls` which results in, among other things, no temp file

--- scripts/.bin/create-ten-latest-ocremix-playlist.sh
+++ scripts/.bin/create-ten-latest-ocremix-playlist.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 TARGET_PLAYLIST_FILE=$1
-TMP_PLAYLIST_FILE="/tmp/playlist"
 OCREMIX_SINGLES_DIR="$HOME/media/music/OCREMIX/singles"
 
-ls -lArt "$OCREMIX_SINGLES_DIR" | tail -n 10 | rev | cut -d ' ' -f 1 | rev | cut -d '*' -f 1 | tac > "$TMP_PLAYLIST_FILE"
-sed -e 's#^#OCREMIX/singles/#' "$TMP_PLAYLIST_FILE" > "$TARGET_PLAYLIST_FILE"
-rm "$TMP_PLAYLIST_FILE"
+# `find` all files in $OCREMIX_SINGLES_DIR and prepend timestamp
+# `sort` by that timestamp
+# take the 10 most recent and cut out the last three '/' seperated fields
+# save in $TARGET_PLAYLIST_FILE
+find "$OCREMIX_SINGLES_DIR" -type f -printf '%T@ %p\n' \
+  | sort -k 1nr \
+  | head -n 10 | rev | cut -d '/' -f -3 | rev > "$TARGET_PLAYLIST_FILE"
