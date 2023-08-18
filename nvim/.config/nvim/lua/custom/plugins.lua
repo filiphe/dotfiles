7b0c83b48ea4472a2492b4cd7125f9ffb8dd6cc8
Filip Hesslund <filip@hesslund.org>
2023-08-18 17:13:46 +0200

    use nvchad

---
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,34 @@
+local plugins = {
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- defaults 
+        "vim",
+        "lua",
+
+        -- web dev 
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+        "json",
+
+        -- modern
+        "go",
+        "rust",
+
+      },
+    },
+  },
+}
+
+return plugins
